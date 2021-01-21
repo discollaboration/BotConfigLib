577ed8e64264843ea2c168e4a40035791b8b1b28
TAG-Epic <tagepicuwu@gmail.com>
2021-01-21 11:38:28 +0000

    Fix: Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="botconfig",
-    version="1.0.0",
+    version="1.0.1",
     description="A simple discord bot configuration utility",
     long_description=open("README.md").read(),
     author="tag-epic",
